1451578c19a60714def901a84288bd789affb537
varaya64 <49073400+varaya64@users.noreply.github.com>
2019-08-07 16:29:51 -0600

    Ventana

---
+++ Tarea-datos/src/Ventana.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import java.util.Scanner;
+
+public class Ventana extends Application {
+	
+	Stage stage;
+	Scene scene;
+	ArrayList<String> archivo = new ArrayList<String> ();
+	
+	
+	public static void main (String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception { //es una ventana
+		
+		stage = primaryStage;
+		stage.setTitle("Lector de Archivos");
+		Button subirAr = new Button("Subir");
+		subirAr.setOnAction(e -> leerArchivo());
+		StackPane contenedor = new StackPane();
+		contenedor.getChildren().add(subirAr);
+		Scene informacion = new Scene(contenedor, 200, 200);
+		stage.setScene(informacion);
+		stage.show();
+		
+	}
+	
+	public void leerArchivo(){
+		String nombreArchivo = "archivo.csv";
+		File abrir = new File(nombreArchivo);
+		
+		try {
+			Scanner leer = new Scanner(abrir);
+			
+			while(leer.hasNext()) {
+				String linea = leer.next();
+				this.archivo.add(linea);
+				System.out.println(linea);
+			}
+
+		}
+		
+		catch(FileNotFoundException i) {
+			System.out.println("Archivo no encontrado");
+	
+		}
+
+	}
+	
+}
